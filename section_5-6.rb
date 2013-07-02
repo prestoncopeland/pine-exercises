f95ec7570e6db881c66a55c1e3ca9f56220d8dd1
Preston Copeland <copelandps1@gmail.com>
2013-07-02 02:08:40 -0400

    first commit from command line

---
+++ section_5-6.rb
@@ -0,0 +1,20 @@
+#June 27, 2013
+#Full name greeting
+firstName = ""
+middleName = ""
+lastName = ""
+
+puts "What is your first name?"
+firstName = gets.chomp
+puts "What is your middle name?"
+middleName = gets.chomp
+puts "What is your last name?"
+lastName = gets.chomp
+puts "Hello, #{firstName} #{middleName} #{lastName}!"
+
+#Bigger, better, favorite number
+favoriteNumber = ""
+puts "What's your favorite number?"
+favoriteNumber = gets.chomp
+biggerNumber = favoriteNumber.to_i + 1
+puts "Perhaps you would prefer #{biggerNumber}?"
